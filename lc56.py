21d952a8aa3a94c8258811d1507abf3e101c050b
anumeric <92189273+anumeric@users.noreply.github.com>
2022-01-04 12:08:58 -0500

    Update lc56.py

--- lc56.py
+++ lc56.py
@@ -5,20 +5,18 @@ class Solution:
             minFinish = min(a[1],b[1])
             return (minFinish >=  maxStart) # overlap
         
-        intervals.sort(key = lambda x:x[0])
+        intervals.sort()
         res = []
         temp = intervals[0]
         res.append(temp)
         
         for i in range(1,len(intervals)):
-            if isOverlap(temp,intervals[i]):
-                res[i-1] = None
+            if isOverlap(temp,intervals[i]): # overlap 
                 minStart = min(temp[0],intervals[i][0])
                 maxFinish = max(temp[1],intervals[i][1])
                 temp = [minStart,maxFinish]
-                res.append(temp)
+                res[-1] = temp
             else:
                 temp = [intervals[i][0],intervals[i][1]]
                 res.append(temp)
-
-        return filter(None,res)
+        return res
