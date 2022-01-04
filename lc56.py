f645b5c858439e20d195a9ba14970acf7495fbf5
anumeric <92189273+anumeric@users.noreply.github.com>
2022-01-04 12:39:40 -0500

    Update lc56.py

--- lc56.py
+++ lc56.py
@@ -11,12 +11,20 @@ class Solution:
         res.append(temp)
         
         for i in range(1,len(intervals)):
-            if isOverlap(temp,intervals[i]): # overlap 
-                minStart = min(temp[0],intervals[i][0])
-                maxFinish = max(temp[1],intervals[i][1])
-                temp = [minStart,maxFinish]
-                res[-1] = temp
+            # IF OVERLAP EXISTS
+            # 1. set temp = overlap
+            # 2. update last entry in res with the overlap
+            if isOverlap(res[-1],intervals[i]): # overlap 
+                minStart = min(res[-1][0],intervals[i][0])
+                maxFinish = max(res[-1][1],intervals[i][1])
+                #temp = [minStart,maxFinish]
+                res[-1] = [minStart,maxFinish]
+                
+            # IF NO OVERLAP EXISTS
+            # 1. set temp = current interval 
+            # 2. add current interval to res
             else:
-                temp = [intervals[i][0],intervals[i][1]]
-                res.append(temp)
+                #temp = [intervals[i][0],intervals[i][1]]
+                res.append([intervals[i][0],intervals[i][1]]) # current interval 
+
         return res
